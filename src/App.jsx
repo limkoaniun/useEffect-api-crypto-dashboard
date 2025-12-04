f95853ff3078882c3c04689b9f21b1b0670e7f33
Guanyu Lin <limkoaniun@gmail.com>
2025-12-04 17:52:11 +1100

    finish coin card

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,48 @@
 import {useEffect, useState} from "react";
+import {CoinCard} from "./components/CoinCard.jsx";
+
 const API_URL = 'https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=10&page=1&sparkline=false';
 
 const App = () => {
-   const [coins, setCoins] = useState([]);
-   const [loading, setLoading] = useState(true);
-   const [error, setError] = useState(null);
+    const [coins, setCoins] = useState([]);
+    const [loading, setLoading] = useState(true);
+    const [error, setError] = useState(null);
 
     useEffect(() => {
-        fetch(API_URL)
-            .then((res) => {
-                if(!res) throw new Error("Failed to fetch data")
-                return res.json();
-            })
-            .then((data) => {
+        const fetchCoins = async () => {
+            try {
+                const res = await fetch(API_URL);
+                if (!res.ok) throw new Error("Failed to fetch data");
+                const data = await res.json();
                 console.log(data);
-                setCoins(data)
-                setLoading(false)
-            })
-            .catch((err) =>{
-                setError(err.message)
-                setLoading(false)
-            })
+                setCoins(data);
+            } catch (err) {
+                setError(err.message);
+            } finally {
+                setLoading(false);
+            }
+        }
+
+        fetchCoins();
     }, []);
-   return (
-      <div>
-          <h1>🚀 Crypto Dash</h1>
-      </div>
-   );
+
+    return (
+        <div>
+            <h1>🚀 Crypto Dash</h1>
+            {loading && <p>loading..</p>}
+            {error && <div className='error'>{error}</div>}
+
+            {!loading && !error && (
+                <main className="grid">
+                    {
+                        coins.map((coin) => (
+                            <CoinCard key={coin.id} coin={coin}/>
+                        ))
+                    }
+                </main>
+            )}
+        </div>
+    );
 }
 
 export default App;
